8a8e81877f1b3fd98989ba02e2a5b9ec3860ff66
张旭策 <xuce.zhang@naxions.com>
2022-12-09 10:40:05 +0800

    style: test类格式化

--- src/test/java/com/example/zxcpractice/sql/TestNGTask1.java
+++ src/test/java/com/example/zxcpractice/sql/TestNGTask1.java
@@ -9,34 +9,33 @@ import org.testng.annotations.*;
 public class TestNGTask1 {
 
     @BeforeMethod
-    public void test3(){
+    public void test3() {
         System.out.println("------beforeMethod------");
     }
 
     @AfterMethod
-    public void test4(){
+    public void test4() {
         System.out.println("------afterMethod------");
     }
 
     @Test
-    public void test1(){
+    public void test1() {
         System.out.println("------test1------");
     }
     @Test
-    public void test2(){
+    public void test2() {
         System.out.println("------test2------");
     }
 
     @BeforeClass
-    public void test5(){
+    public void test5() {
         System.out.println("------类之前运行------");
     }
 
     @AfterClass
-    public void test6(){
+    public void test6() {
         System.out.println("------类之后运行------");
     }
-
 
 
 }
